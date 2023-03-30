69491ba67bcc0cb7a1ca1ca30dd28a2c393d368e
rouille397 <p.rouille397@gmail.com>
2023-03-30 10:21:00 +0200

    first commit

---
+++ addresses.ts
@@ -0,0 +1,3 @@
+// Use your marketplace contract address here
+export const marketplaceContractAddress = '0x845CDbBB8F6C62878076455Ef48a0b1bd6F53d4F';
+export const Marketplace = '0x845CDbBB8F6C62878076455Ef48a0b1bd6F53d4F';
