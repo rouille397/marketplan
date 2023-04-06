5841dc4917faf839a735b5db4c0ea80240f707c5
rouille397 <83870437+rouille397@users.noreply.github.com>
2023-04-06 09:12:35 +0200

    Update addresses.ts

--- addresses.ts
+++ addresses.ts
@@ -1,3 +1,3 @@
 // Use your marketplace contract address here
-export const marketplaceContractAddress = '0x845CDbBB8F6C62878076455Ef48a0b1bd6F53d4F';
-export const Marketplace = '0x845CDbBB8F6C62878076455Ef48a0b1bd6F53d4F';
+export const marketplaceContractAddress = '0x2E4A0d0A0deD36b878Fa6BD56a5d537a682e4991';
+export const Marketplace = '0x2E4A0d0A0deD36b878Fa6BD56a5d537a682e4991';
