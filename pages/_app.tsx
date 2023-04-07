c561138806932e0e1dfea2be4b6179ecf4d79bbe
rouille397 <83870437+rouille397@users.noreply.github.com>
2023-04-07 19:55:48 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,7 +12,10 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>Nitfee Marketplace</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-       
+        <meta
+          name="description"
+          content="Learn How To Use Thirdweb's Marketplace with Next.JS To List Your NFTs For Sale, Accept Bids, and Buy NFTs"
+        />
         <meta
           name="keywords"
           content="Thirdweb, Marketplace, NFT Marketplace Tutorial, NFT Auction Tutorial, How To Make OpenSea"
