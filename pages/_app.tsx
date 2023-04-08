d5a6b54a9300d510351b3f8ca7c975edaaa37477
rouille397 <83870437+rouille397@users.noreply.github.com>
2023-04-08 19:49:22 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,25 +2,15 @@ import type { AppProps } from "next/app";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import { ConfluxEspace } from "@thirdweb-dev/chains";
 import "../styles/globals.css";
-import Head from "next/head";
 import Header from "../components/Header";
 
 // This is the chainId your dApp will work on.
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider activeChain={ConfluxEspace}>
-      <Head>
+  
         <title>Nitfee Marketplace</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta
-          name="description"
-          content="List Your NFTs For Sale, Accept Bids, and Buy NFTs"
-        />
-        <meta
-          name="keywords"
-          content="Thirdweb, Marketplace, NFT Marketplace Tutorial, NFT Auction Tutorial, How To Make OpenSea"
-        />
-      </Head>
+  
       <Header />
       <Component {...pageProps} />
     </ThirdwebProvider>
