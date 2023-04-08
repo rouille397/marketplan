47b8cb4da02d301be62005d1dd49857b7f999846
rouille397 <83870437+rouille397@users.noreply.github.com>
2023-04-08 19:53:52 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,9 +8,18 @@ import Header from "../components/Header";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider activeChain={ConfluxEspace}>
-  
+      <Head>
         <title>Nitfee Marketplace</title>
-  
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="List Your NFTs For Sale, Accept Bids, and Buy NFTs"
+        />
+        <meta
+          name="keywords"
+          content="Thirdweb, Marketplace, NFT Marketplace Tutorial, NFT Auction Tutorial, How To Make OpenSea"
+        />
+      </Head>
       <Header />
       <Component {...pageProps} />
     </ThirdwebProvider>
@@ -18,4 +27,3 @@ function MyApp({ Component, pageProps }: AppProps) {
 }
 
 export default MyApp;
-
